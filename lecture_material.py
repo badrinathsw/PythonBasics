9315059fca564bb278019da2c9f68c8a2199fd09
Badri Nath <badri.nath.sw@gmail.com>
2024-10-19 08:51:04 +0530

    Initial commit

---
+++ lecture_material.py
@@ -0,0 +1,39 @@
+
+print("Hello World")
+var_1 = 4
+
+print(var_1)
+
+var_2 = True
+var_3 = 3.1
+var_1 = 5
+
+print(var_2)
+print(var_3)
+print(var_1)
+#This is comment
+
+addition = 4 + 5
+subtraction = 5 - 2
+division = 7/2
+multiplication = 3*2
+exponentiation = 4**4
+floor_division = 16//5
+modulo = 7 % 3
+
+print(addition)
+print(subtraction)
+print(division)
+print(multiplication)
+print(exponentiation)
+print(floor_division)
+print(modulo)
+
+#Assignment Operator
+add_assign = 5
+print(add_assign)
+add_assign += 7
+print(add_assign)
+print(4 + 5 - 2)
+#Precedence of operators () -> ** -> %, /, //,* -> +, -
+
